a5c533b1e2eac069e5785f621c2c1c6132d13487
Natali Schers <schersnatali@gmail.com>
2025-12-19 23:05:45 -0300

    ♻️ refactor: update ContaCorrente payment logic and enhance error messages

--- banco/bin/banco.dart
+++ banco/bin/banco.dart
@@ -5,18 +5,10 @@ void main() {
 
   Conta conta1 = Conta('Alice', 1500.0);
   Conta conta2 = Conta('Bob', 2500.5);
-  ContaCorrente conta3 = ContaCorrente('Carlos', 3000.0);
+  ContaCorrente conta3 = ContaCorrente('Carlos', 1000.0);
   ContaPoupanca conta4 = ContaPoupanca('Diana', 4000.0);
 
-  contas.add(conta1);
-  contas.add(conta2);
+  conta3.enviarPagamento(1400.0);
 
-  for (var conta in contas) {
-    conta.imprimirSaldo();
-  }
-
-  conta1.receberPagamento(500.0);
-  conta2.enviarPagamento(300.0);
-  conta3.enviarPagamento(3000.0);
-  conta4.receberPagamento(750.0);
+  conta4.calcularRendimento();
 }
--- banco/lib/conta.dart
+++ banco/lib/conta.dart
@@ -19,16 +19,35 @@ class Conta {
       imprimirSaldo();
     } else {
       print(
-        'Saldo insuficiente para enviar pagamento de $valor de $titular',
+        'Saldo insuficiente para enviar pagamento de $valor de $titular!',
       );
     }
   }
 }
 
 class ContaCorrente extends Conta {
+  double emprestimoDisponivel = 300.0;
+
   ContaCorrente(super.titular, super.saldo);
+
+  @override
+  void enviarPagamento(double valor) {
+    if (_saldo + emprestimoDisponivel >= valor) {
+      _saldo -= valor;
+      imprimirSaldo();
+    } else {
+      print('Saldo insuficiente para enviar pagamento de $valor de $titular!');
+    }
+  }
 }
 
 class ContaPoupanca extends Conta {
+  double rendimento = 0.05;
+
   ContaPoupanca(super.titular, super.saldo);
+
+  void calcularRendimento() {
+    _saldo += _saldo * rendimento;
+    imprimirSaldo();
+  }
 }
