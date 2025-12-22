2f4ab1f3964294bb43f91feda06595f849e5720e
Natali Schers <schersnatali@gmail.com>
2025-12-21 22:23:29 -0300

    ✨ feat: implement ContaEmpresa and ContaInvestimento classes with tax handling for payments

--- banco/bin/banco.dart
+++ banco/bin/banco.dart
@@ -1,14 +1,20 @@
 import 'package:banco/conta.dart';
 
 void main() {
-  List<Conta> contas = <Conta>[];
-
-  Conta conta1 = Conta('Alice', 1500.0);
-  Conta conta2 = Conta('Bob', 2500.5);
+  ContaEmpresa conta1 = ContaEmpresa('Alice', 5000.0);
+  ContaInvestimento conta2 = ContaInvestimento('Bob', 3000.0);
   ContaCorrente conta3 = ContaCorrente('Carlos', 1000.0);
   ContaPoupanca conta4 = ContaPoupanca('Diana', 4000.0);
 
-  conta3.enviarPagamento(1400.0);
+  conta1.enviarPagamento(1000.0);
+  conta1.receberPagamento(1500.0);
+
+  conta2.enviarPagamento(500.0);
+  conta2.receberPagamento(800.0);
+
+  conta3.enviarPagamento(1200.0);
+  conta3.receberPagamento(300.0);
 
   conta4.calcularRendimento();
+  conta4.enviarPagamento(500.0);
 }
--- banco/lib/conta.dart
+++ banco/lib/conta.dart
@@ -1,4 +1,4 @@
-class Conta {
+abstract class Conta {
   String titular;
   double _saldo;
 
@@ -48,6 +48,62 @@ class ContaPoupanca extends Conta {
 
   void calcularRendimento() {
     _saldo += _saldo * rendimento;
+    imprimirSaldo();
+  }
+}
+
+mixin Imposto {
+  double taxa = 0.03;
+
+  double calcularImposto(double valor) {
+    return valor * taxa;
+  }
+}
+
+class ContaEmpresa extends Conta with Imposto {
+  ContaEmpresa(super.titular, super._saldo);
+
+  @override
+  void enviarPagamento(double valor) {
+    double valorComImposto = valor + calcularImposto(valor);
+    if (_saldo >= valorComImposto) {
+      _saldo -= valorComImposto;
+      imprimirSaldo();
+    } else {
+      print(
+        'Saldo insuficiente para enviar pagamento de $valor (com imposto) de $titular!',
+      );
+    }
+  }
+
+  @override
+  void receberPagamento(double valor) {
+    double valorComImposto = valor - calcularImposto(valor);
+    _saldo += valorComImposto;
+    imprimirSaldo();
+  }
+}
+
+class ContaInvestimento extends Conta with Imposto {
+  ContaInvestimento(super.titular, super._saldo);
+
+  @override
+  void enviarPagamento(double valor) {
+    double valorComImposto = valor + calcularImposto(valor);
+    if (_saldo >= valorComImposto) {
+      _saldo -= valorComImposto;
+      imprimirSaldo();
+    } else {
+      print(
+        'Saldo insuficiente para enviar pagamento de $valor (com imposto) de $titular!',
+      );
+    }
+  }
+
+  @override
+  void receberPagamento(double valor) {
+    double valorComImposto = valor - calcularImposto(valor);
+    _saldo += valorComImposto;
     imprimirSaldo();
   }
 }
