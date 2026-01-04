7eaea9a098550f6c7cc29c5aeff05fed34a3f467
Natali Schers <schersnatali@gmail.com>
2026-01-04 00:55:30 -0300

    ✨ feat: update data fetching and sending logic with API key integration

--- assincronismo/bin/main.dart
+++ assincronismo/bin/main.dart
@@ -1,20 +1,52 @@
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:assincronismo/api_key.dart';
 
 void main() {
   print("Hello, world!");
 
-    requestDataAsync();
+  //requestDataAsync();
+
+  sendDataAsync({
+    "id": "ID011",
+    "name": "Natali",
+    "lastName": "Schers",
+    "balance": 2500.0,
+  });
 }
 
-void requestDataAsync() async {
-  String url = "https://gist.githubusercontent.com/natali-schers/e5f0dd30eb09d19856ea7b8a5239742f/raw/9c73ee9f30f22a47bd39201cb0e37656cd4beb9a/accounts.json";
+Future<List<dynamic>> requestDataAsync() async {
+  String url =
+      "https://gist.githubusercontent.com/natali-schers/e5f0dd30eb09d19856ea7b8a5239742f/raw/9c73ee9f30f22a47bd39201cb0e37656cd4beb9a/accounts.json";
 
   Response response = await get(Uri.parse(url));
 
   List<dynamic> accounts = json.decode(response.body);
 
-  for (var account in accounts) {
-    print("Name: ${account["name"]} - Balance: ${account["balance"]}");
-  }
+  return accounts;
+}
+
+void sendDataAsync(Map<String, dynamic> data) async {
+  List<dynamic> accounts = await requestDataAsync();
+
+  accounts.add(data);
+
+  String encondedData = json.encode(accounts);
+
+  String url = "https://api.github.com/gists/e5f0dd30eb09d19856ea7b8a5239742f";
+
+  Response response = await post(
+    Uri.parse(url),
+    headers: {"Authorization": "Bearer $githubApiKey"},
+    body: json.encode({
+      "description": "Updated accounts data",
+      "public": true,
+      "files": {
+        "accounts.json": {"content": encondedData}
+      }
+    }),
+  );
+
+  print(response.body);
+  print(response.statusCode);
 }
