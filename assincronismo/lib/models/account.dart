21f48cf2b44ccefdd2ad8d3c23eef18161456d72
Natali Schers <schersnatali@gmail.com>
2026-01-11 23:03:20 -0300

    ✨ feat: enhance Account model with JSON serialization

--- assincronismo/lib/models/account.dart
+++ assincronismo/lib/models/account.dart
@@ -1,10 +1,17 @@
+import 'dart:convert';
+
 class Account {
   String id;
   String name;
   String lastName;
   double balance;
 
-  Account({required this.id, required this.name, required this.lastName, required this.balance});
+  Account({
+    required this.id,
+    required this.name,
+    required this.lastName,
+    required this.balance,
+  });
 
   factory Account.fromMap(Map<String, dynamic> map) {
     return Account(
@@ -22,5 +29,45 @@ class Account {
       'lastName': lastName,
       'balance': balance,
     };
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Account.fromJson(String source) =>
+      Account.fromMap(json.decode(source));
+
+  Account copyWith({
+    String? id,
+    String? name,
+    String? lastName,
+    double? balance,
+  }) {
+    return Account(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      lastName: lastName ?? this.lastName,
+      balance: balance ?? this.balance,
+    );
+  }
+
+  @override
+  String toString() {
+    return 'Conta $id: $name $lastName, Saldo: $balance';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Account &&
+        other.id == id &&
+        other.name == name &&
+        other.lastName == lastName &&
+        other.balance == balance;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^ name.hashCode ^ lastName.hashCode ^ balance.hashCode;
   }
 }
